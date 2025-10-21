ebd239d3c4ddecf443f9972e45c10657fd3ed106
HHabhab <hi0224@wayne.edu>
2025-10-20 20:30:42 -0400

    combined sql files and edited table

--- server/sql/003_users.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    email TEXT NOT NULL UNIQUE,
-    name TEXT NOT NULL,
-    role TEXT NOT NULL CHECK (role IN ('Viewer','Worker','Manager','Admin')),
-    password_hash TEXT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
-/*paste in query tool and execute*/
